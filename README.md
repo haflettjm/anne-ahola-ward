42600579d849b7e2b8e05702ed04e763d348de32
hafletjm01 <jacobhaflett@Jacobs-MBP.attlocal.net>
2022-04-06 13:35:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Website Prototype for Anne Ahola Ward
+
+Working with:
+Tailwind CSS
+NextJS
+Vanta
+Node
+
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Anne Ahola Ward</h1>
+    </div>
+  )
+}
